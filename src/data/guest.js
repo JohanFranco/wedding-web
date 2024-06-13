28ab04c00be75c282d1690c27e1decb91be450db
JOHAN <johan_franco@pegaduro.com.mx>
2024-06-13 12:27:34 -0600

    Invite

--- src/data/guest.js
+++ src/data/guest.js
@@ -133,6 +133,76 @@
         "fullName": "JOHAN FRANCO",
         "phone": "3334134159",
         "tickets": "2"
-    }
+    },
+    {
+        "fullName": "EVENCIO BURGARA",
+        "phone": "3318953818",
+        "tickets": "2"
+    },
+    {
+        "fullName": "OSWALDO ALDRETE",
+        "phone": "3334688754",
+        "tickets": "2"
+    },
+    {
+        "fullName": "ROGELIO CLEMENTE",
+        "phone": "3314942685",
+        "tickets": "2"
+    },
+    {
+        "fullName": "JOHANA JUREGUI",
+        "phone": "3314196173",
+        "tickets": "2"
+    },
+    {
+        "fullName": "JOSE LUIS MARTINEZ",
+        "phone": "3326197021",
+        "tickets": "2"
+    },
+    {
+        "fullName": "OMAR GUZMAN (TERRONCITO <3)",
+        "phone": "3313476970",
+        "tickets": "1"
+    },
+    {
+        "fullName": "RODRIGO ROBLES",
+        "phone": "3318958738",
+        "tickets": "2"
+    },
+    {
+        "fullName": "JESSICA ROBLES",
+        "phone": "3322561361",
+        "tickets": "2"
+    },
+    {
+        "fullName": "OSMAR ROBLES",
+        "phone": "3313198483",
+        "tickets": "5"
+    },
+    {
+        "fullName": "ARIEL ROBLES",
+        "phone": "3331307104",
+        "tickets": "2"
+    },
+    {
+        "fullName": "CHANO ROBLES",
+        "phone": "3313956199",
+        "tickets": "2"
+    },
+    {
+        "fullName": "NAPOLEÓN",
+        "phone": "3318035718",
+        "tickets": "2"
+    },
+    {
+        "fullName": "ANGEL DÍAZ",
+        "phone": "3311775689",
+        "tickets": "2"
+    },
+    {
+        "fullName": "SACNICTEC",
+        "phone": "3335769191",
+        "tickets": "2"
+    },
 
 ]
--- src/pages/api/message.ts
+++ src/pages/api/message.ts
@@ -9,7 +9,7 @@ const verifySchema = object({
 
 export const POST: APIRoute = async ({ request }) => {
   const { success, output } = safeParse(verifySchema, await request.json());
-  if (!success) return new Response("Bad Request", { status: 400 });
+  if (!success) return new Response("Body content is not allowed", { status: 400 });
 
   const { phone, url, fullName } = output;
   const message = `Buen día ${fullName}, te saludan Magaly y Rene. En el siguiente link puedes consultar tu invitación para nuestra boda ${url}`;
@@ -17,13 +17,13 @@ export const POST: APIRoute = async ({ request }) => {
   try {
 
     const twilio = await import('twilio'); 
-    // const client = twilio.default('', '');
+    const client = twilio.default('AC7711786f3f35463979f87ea3c3db5fa6', '5ccf9ba2970fd9f2dc6291dfcc7a9272');
 
-    // await client.messages.create({
-    //   body: message,
-    //   from: 'whatsapp:+14155238886', 
-    //   to: `whatsapp:+521${phone}`
-    // });
+    await client.messages.create({
+      body: message,
+      from: 'whatsapp:+14155238886', 
+      to: `whatsapp:+521${phone}`
+    });
 
     return new Response("Message has been sent", { status: 200 });
   } catch (err) {
